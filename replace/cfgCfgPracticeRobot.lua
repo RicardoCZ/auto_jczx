a5be4cdbe6711317b282962e8ca85128b67af663
RicardoCZ <1921233554@qq.com>
2024-10-17 11:17:05 +0800

    t commit

---
+++ replace/cfgCfgPracticeRobot.lua
@@ -0,0 +1,14 @@
+local conf = {
+	["filename"] = 'j-军演.xlsx',
+	["sheetname"] = '机器人',
+	["types"] = {
+'int','string','string','int','int','int','int','int','int[]'
+},
+	["names"] = {
+'id','key','nName','nLevel','nScore','nPerformance','nIconId','nGridId','aTeamIds'
+},
+	["data"] = {
+},
+}
+--cfgCfgPracticeRobot = conf
+return conf
